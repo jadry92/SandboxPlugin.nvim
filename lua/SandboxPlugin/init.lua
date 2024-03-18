5fdc7f1890a372eb82243d2bf414b7456cac0a80
Johan Suarez <jhoanseb1992@gmail.com>
2024-03-18 20:55:05 +1100

    add window example

--- lua/SandboxPlugin/init.lua
+++ lua/SandboxPlugin/init.lua
@@ -1,11 +1,12 @@
 local M = {}
 
-function M.setup(config)
-	print(config)
+function my_print()
+	vim.api.nvim_open_win(0, false,
+		{ relative = 'win', width = 12, height = 3, bufpos = { 100, 10 } })
 end
 
-function M.my_print()
-	print("Hello from my_print")
+function M.setup(config)
+	vim.api.nvim_create_user_command("MyPrint", my_print, {})
 end
 
 return M
