14207da5fc1ade2c286965f413f5f32ffc56476a
johan suarez <jhoanseb1992@gmail.com>
2024-03-19 12:15:32 +1100

    add float views

--- lua/SandboxPlugin/init.lua
+++ lua/SandboxPlugin/init.lua
@@ -1,8 +1,15 @@
 local M = {}
 
-function my_print()
-	vim.api.nvim_open_win(0, false,
-		{ relative = 'win', width = 12, height = 3, bufpos = { 100, 10 } })
+local function my_print()
+	local win = vim.api.nvim_get_current_win()
+	local width = math.floor(vim.api.nvim_win_get_width(win) / 2)
+	local height = math.floor(vim.api.nvim_win_get_height(win))
+	local help_buffer = vim.api.nvim_create_buf(false, true)
+	local new_buffer = vim.api.nvim_create_buf(false, true)
+	vim.api.nvim_open_win(help_buffer, false,
+		{ relative = 'win', width = width, height = 20, bufpos = { 0, 0 }, border = 'single' })
+	vim.api.nvim_open_win(new_buffer, true,
+		{ relative = 'win', width = width, height = height - 21, bufpos = { 20, 0 }, border = 'single' })
 end
 
 function M.setup(config)
